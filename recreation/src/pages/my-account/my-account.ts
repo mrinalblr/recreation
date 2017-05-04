b59b07b0bd0195c6e583cc449de74585f2ed9338
Mrinal Deo (COMMUNICATIONS) <ME765564@wipro.com>
2017-05-04 17:56:22 +0530

    Updates on UI

--- recreation/src/pages/my-account/my-account.ts
+++ recreation/src/pages/my-account/my-account.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ActionSheetController, ToastController, AlertController} from 'ionic-angular';
+import { Camera } from 'ionic-native';
+import { DomSanitizer } from '@angular/platform-browser';
 
 /*
   Generated class for the MyAccount page.
@@ -13,10 +15,58 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class MyAccountPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  private imageSelected: boolean
+  public base64Image: string
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    public toastController: ToastController,
+    public actionSheetCtrl: ActionSheetController,
+    public alertController: AlertController,
+    public _DomSanitizationService: DomSanitizer) {
+        this.base64Image = "assets/images/avatar_user_image.png";
+     }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MyAccountPage');
   }
+  public openFileChooser() {
 
+    const actionSheet = this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: 'Choose Photo',
+          handler: () => {
+            this.getPicture(0); // 0 == Library
+          }
+        },
+        {
+          text: 'Take Photo',
+          handler: () => {
+            this.getPicture(1); // 1 == Camera
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    });
+    actionSheet.present();
+
+  }
+  getPicture(sourceType: number) {
+    Camera.getPicture({
+      quality: 100,
+      destinationType: 0, // DATA_URL
+      sourceType,
+      allowEdit: true,
+      saveToPhotoAlbum: false,
+      correctOrientation: true
+    }).then((imageData) => {
+      this.base64Image = `data:image/jpeg;base64,${imageData}`;
+      console.log("BASE64 : " + this.base64Image);
+      this.imageSelected = true;
+    }, (err) => {
+      console.log(`ERROR -> ${JSON.stringify(err)}`);
+    });
+  }
 }
